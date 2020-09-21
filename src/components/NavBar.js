f147075e04f9c8bb918a184a28bac1b91c3023a8
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 20:28:02 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,43 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+    
+      <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+     <NavLink
+      to="/movies"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Movies</NavLink>
+     <NavLink
+      to="/directors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Directors</NavLink>
+     <NavLink
+      to="/actors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Actors</NavLink>
     </div>
   );
 };
 
+
 export default NavBar;
+
